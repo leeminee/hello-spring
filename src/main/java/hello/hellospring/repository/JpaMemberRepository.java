a0fe4b4f54c40ed7a8020736842aeeca686f4b04
이민이 <mllee@ipageon.com>
2020-10-16 17:08:14 +0900

    JPA 까지

--- src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -28,13 +28,14 @@ public class JpaMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findByName(String name) {
-//        em.createQuery("select m from Member m");
-        return Optional.empty();
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
-        return result;
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 }
